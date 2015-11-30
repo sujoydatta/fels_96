bac89a72d6a48102fd36dc3687765cc885e2864c
MuhammadTamzid <muhammad.tamzid@gmail.com>
2015-11-30 17:43:21 +0600

    create category

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
     get "home"    => "static_pages#home"
     get "about"   => "static_pages#about"
     get "contact" => "static_pages#contact"
+
+    namespace :admin do
+      root "categories#index"
+      resources :categories
+    end
   end
 end
