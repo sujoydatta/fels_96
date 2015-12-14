1cf5dfed20e7e15c87564555187d8df22ccdf615
Sujoy Datta <sujoydatta25@yahoo.com>
2015-12-14 17:21:17 +0600

    spec for category model

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+describe Category do
+  it "has a valid factory" do
+    FactoryGirl.create(:category).should be_valid
+  end
+
+  it "is invalid without a name" do
+    FactoryGirl.build(:category, name: nil).should_not be_valid
+  end
+end
