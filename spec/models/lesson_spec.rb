43313790ee0c4a6695282a7d2eb2b5c72d9000ee
SSalekin <sjsalekin@gmail.com>
2015-12-21 15:56:05 +0600

    Update the user view, refactor old codes

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe Lesson do
   it "has a valid factory" do
-    FactoryGirl.create(:lesson).should be_valid
+    expect(FactoryGirl.create :lesson).to be_valid
   end
 
   it {should have_many :lesson_words}
